a1f08ac81e7977e08c8f7e9258d9531065f540ac
Eliran Eiluz <eliranfr1@gmail.com>
2021-11-21 20:09:25 +0200

    update client and server

--- client.py
+++ client.py
@@ -246,8 +246,8 @@ class Handler(FileSystemEventHandler):
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((ip, port))
-protocol = user_identifier + "," + "999" + ",0"
-protocol_sender(s, protocol)
+start_protocol = user_identifier + "," + "999" + ",0"
+protocol_sender(s, start_protocol)
 
 # In case we are new client.
 # TODO deal with folder names of ","
