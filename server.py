93aabb2f25c86594824b46a4a147c3bc33d687f7
Eliran Eiluz <eliranfr1@gmail.com>
2021-12-07 21:01:17 +0200

    update client and server

--- server.py
+++ server.py
@@ -84,10 +84,12 @@ def event(sock):
             if comp_id != pc_id:
                 change_list.append(event_data)
     if event_type == "created":
+        sock.send(int(0).to_bytes(1, 'big'))
         created_event(sock, file_type, os.getcwd(), path)
     elif event_type == "deleted":
         deleted_event(file_type, os.getcwd(), path)
     elif event_type == "modified":
+        sock.send(int(0).to_bytes(1, 'big'))
         modified_event(sock, os.getcwd(), path)
     else:
         moved_event(file_type, os.getcwd(), path)
